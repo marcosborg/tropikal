778f84b6803c9f53b495bd2d5e9fefbad13b679e
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2026-08-16 10:24:34 +0100

    Remove atualizador temporário

--- tests/Feature/DeploymentTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Tests\Feature;
-use Illuminate\Foundation\Testing\RefreshDatabase; use Tests\TestCase;
-class DeploymentTest extends TestCase { use RefreshDatabase;
- public function test_deployment_form_is_available_and_rejects_invalid_key():void { $this->get('/manutencao/atualizar-tropikal-2026')->assertOk()->assertSee('Atualização Tropikal'); $this->post('/manutencao/atualizar-tropikal-2026',['deployment_key'=>'errada'])->assertForbidden(); }
-}
